8088658bed60c04b705892eb9d7b975119e47687
Jacob B <pokeborg3@gmail.com>
2019-07-18 17:14:06 -0700

    started the development of the dev center

---
+++ dev/secure.js
@@ -0,0 +1,24 @@
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your JBP Pin',' ');
+while (testV < 3) {
+if (!pass1)
+history.go(-1);
+if (pass1.toLowerCase() == "0000") {
+alert('You Got it Right!');
+window.open('www.wikihow.com');
+break;
+}
+if (pass1.toLowerCase() == "1111") {
+alert('You Got it Right!');
+window.open('./devcenter.html');
+break;
+}
+testV+=1;
+var pass1 =
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3)
+history.go(-1);
+return " ";
+}
