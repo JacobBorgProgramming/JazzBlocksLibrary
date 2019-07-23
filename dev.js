4f089d8216e6c26a822113e91ea98f61e2581351
Jacob B <pokeborg3@gmail.com>
2019-07-22 19:50:53 -0700

    Updated dev assets

--- dev.js
+++ dev.js
@@ -1,2 +1,18 @@
 var gib = "gib"
 //document.getElementById("jsC1").innerHTML = "jacobborgprogramming";
+if (navigator.cookieEnabled === false){
+  console.log('Cookies are disabled')
+  document.getElementById("jsC1").innerHTML = "Your cookies are disabled.";
+  document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
+}
+if (navigator.cookieEnabled === true){
+  document.getElementById("jsC1").innerHTML = "Your cookies are enabled.";
+  document.getElementById("jsC2").innerHTML = "You can change this in your browser's settings.";
+  console.log('Cookies are enabled')
+}
+console.log('JacobBorgProgramming 2019: Cookie Checker')
+/*
+var element = document.createElement('h1');
+element.textContent = "Hello, World";
+document.body.appendChild(element);
+*/
